3c79d9c00cfe1b819f765d45a2c997e3c1a11db4
iancooper <ian_hammond_cooper@yahoo.co.uk>
2017-05-05 10:28:14 +0100

    Working on dispatcher-performer model

--- serviceactivator/dispatch.py
+++ serviceactivator/dispatch.py
@@ -28,54 +28,73 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ***********************************************************************
 """
-from multiprocessing import Array, Event, Process, Queue
+from multiprocessing import Event, Process, Queue
+from typing import Callable
 
 from arame.gateway import ArameConnection, ArameConsumer
 from core.channels import Channel
+from core.command_processor import CommandProcessor, Request
 from core.message_factory import create_quit_message
-from core.messaging import BrightsideConsumerConfiguration
+from core.messaging import BrightsideConsumerConfiguration, BrightsideMessage
 from serviceactivator.message_pump import MessagePump
 
 
 class Performer:
-    def __init__(self, channel_name: str, connection: ArameConnection, consumer_configuration: BrightsideConsumerConfiguration) -> None:
+    def __init__(self,
+                 channel_name: str,
+                 connection: ArameConnection,
+                 consumer_configuration: BrightsideConsumerConfiguration,
+                 command_processor_factory: Callable[[str], CommandProcessor],
+                 mapper_func: Callable[[BrightsideMessage], Request]
+                 ) -> None:
         """
         Each Performer abstracts a process running a message pump.
         That process is forked from the parent, as we cannot guarantee a message pump is only I/O bound and thus will
         not scale because of the GIL.
         The Performer is how the supervisor (the dispatcher) tracks the workers it has created
         The Performer needs:
-            A queue, whose purpose is to allow stop messages to be sent to the channel
-            The properties needed to create a message pump or channel in the run method (which executes in the
-            separate process) that can be communicatd to the child process via shared memory
+        :param channel_name: The name of the channel we want to create a sub-process for
+        :param connection: The connection to the broker
         """
+        #TODO: The paraemter needs to be a connection, not an AramaConnection as we can't decide to create an Arame Consumer
+        # here. Where do we make that choice?
+
         self._pipeline = Queue()
         self._channel_name = channel_name
         self._connection = connection
         self._consumer_configuration = consumer_configuration
+        self._command_processor_factory = command_processor_factory
+        self._mapper_func = mapper_func
 
     def stop(self) -> None:
         self._pipeline.put(create_quit_message())
 
     def run(self) -> Process:
-        event = Event()
-        p = Process(target=_sub_process_main, args=(event, self._pipeline, self._channel_name, self._connection, self._consumer_configuration))
+        started_event = Event()
+        p = Process(target=_sub_process_main, args=(
+            started_event, self._pipeline, self._channel_name, self._connection, self._consumer_configuration,
+        self._command_processor_factory,
+        self._mapper_func))
         p.start()
 
-        event.wait(timeout=1)
+        started_event.wait(timeout=1)
 
         return p
 
 
-def _sub_process_main(event: Event, pipeline: Queue, channel_name: str, connection: ArameConnection,
-                     consumer_configuration: BrightsideConsumerConfiguration) -> None:
+def _sub_process_main(started_event: Event, pipeline: Queue,
+                      channel_name: str,
+                      connection: ArameConnection,
+                      consumer_configuration: BrightsideConsumerConfiguration,
+                      command_processor_factory: Callable[[str], CommandProcessor],
+                      mapper_func: Callable[[BrightsideMessage], Request]) -> None:
     """
     This is the main method for the sub=process, everything we need to create the message pump and
     channel it needs to be passed in as parameters that can be pickled as when we run they will be serialized
     into this process. The data should be value types, not reference types as we will receive a copy of the original.
     Inter-process communication is signalled by the event - to indicate startup - and the pipeline to facilitate a
     sentinel or stop message
-    :param event: Used by the sub-process to signal that it is ready
+    :param started_event: Used by the sub-process to signal that it is ready
     :param pipeline: Used to communicates 'stop' messages to the pump
     :param channel_name: The name we want to give the channel to the broker for identification
     :param connection: The 'broker' connection
@@ -88,6 +107,9 @@ def _sub_process_main(event: Event, pipeline: Queue, channel_name: str, connecti
     channel = Channel(name=channel_name, consumer=consumer, pipeline=pipeline)
 
     #TODO: Fix missing dependencies, including defaults that need passed in config values
-    message_pump = MessagePump(command_processor=None, channel=channel, mapper_func=None,
+    command_processor = command_processor_factory(channel_name)
+    message_pump = MessagePump(command_processor=command_processor, channel=channel, mapper_func=mapper_func,
                                timeout=500, unacceptable_message_limit=-1, requeue_count=3)
+
+    message_pump.run(started_event)
 
