03b63b390d76b98f3f3108f2d191ed429e4c6abe
iancooper <ian_hammond_cooper@yahoo.co.uk>
2017-05-09 23:28:11 +0100

    Moving to a generic connection, creating dispatcher

---
+++ core/connection.py
@@ -0,0 +1,40 @@
+# Create Connection, not Arame Connection
+class ArameConnection:
+    """Contains the details required to connect to a RMQ broker: the amqp uri and the exchange"""
+    def __init__(self, amqp_uri: str, exchange: str, exchange_type: str = "direct", is_durable: bool = False) -> None:
+        self._amqp_uri = amqp_uri
+        self._exchange = exchange
+        self._exchange_type = exchange_type
+        self._is_durable = is_durable
+
+    @property
+    def amqp_uri(self) -> str:
+        return self._amqp_uri
+
+    @amqp_uri.setter
+    def amqp_uri(self, value: str):
+        self._amqp_uri = value
+
+    @property
+    def exchange(self) -> str:
+        return self._exchange
+
+    @exchange.setter
+    def exchange(self, value: str):
+        self._exchange = value
+
+    @property
+    def exchange_type(self) -> str:
+        return self._exchange_type
+
+    @exchange_type.setter
+    def exchange_type(self, value: str):
+        self._exchange_type = value
+
+    @property
+    def is_durable(self):
+        return self._is_durable
+
+    @is_durable.setter
+    def is_durable(self, value):
+        self._is_durable = value
