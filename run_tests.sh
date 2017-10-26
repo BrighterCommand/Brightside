ce55a402101bf5c5f214b457ca63f51f86293090
ian <ian@huddle.com>
2017-10-26 14:42:35 +0100

    Now closes containers when done

--- run_tests.sh
+++ run_tests.sh
@@ -2,3 +2,5 @@
 
 docker-compose -f tests-docker-compose.yml up -d --build
 docker exec -it $(docker ps -aqf "name=brightside-tests") python testrunner.py
+docker-compose -f tests-docker-compose.yml down
+
