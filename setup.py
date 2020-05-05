257992dc8c5f599d11159661f5be25d1acb105de
toby.henderson <toby.henderson@huddle.com>
2020-05-05 16:07:26 +0100

    update version to 0.6.13

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/BrighterCommand/Brightside'
 EMAIL = 'ian_hammond_cooper@yahoo.co.uk'
 AUTHOR = 'Ian Cooper'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.6.12'
+VERSION = '0.6.13'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
